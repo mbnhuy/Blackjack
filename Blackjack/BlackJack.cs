261065c5efcdffa3138b9badb8e154f08b0ecef7
Brandon Veltre <bveltre@sdsu.edu>
2018-11-14 12:29:36 -0800

    Added new GamePage, buttons, BlackJack class, navigation pan (Brandon Veltre)

---
+++ Blackjack/BlackJack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+    class BlackJack
+    {
+        // TODO: Add methods and member variables needed to organize the card game aspect of the program
+    }
+}
--- Blackjack/MainPage.xaml.cs
+++ Blackjack/MainPage.xaml.cs
@@ -26,5 +26,28 @@ namespace Blackjack
         {
             this.InitializeComponent();
         }
+
+        /// <summary>
+        /// This routinue will be triggered when the user presses the Start button from the main menu.
+        /// It will change the screen from the main menu to the main game screen where the user can begin playing 
+        /// the game. Need to look up the code which will allow switching between views in c#.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void StartGame(object sender, RoutedEventArgs e)
+        {
+            // TODO: Write the method which will change to the GamePage
+            this.Frame.Navigate(typeof(GamePage));
+        }
+
+        /// <summary>
+        /// This method will exit the game when the user clicks it from the main menu.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void QuitGame(object sender, RoutedEventArgs e)
+        {
+            // TODO: Write the method to exit the program
+        }
     }
 }
