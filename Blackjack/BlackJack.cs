9ccf137f24c4115e46c8b83d22c7bda5fd5131f0
bveltre <baveltre@gmail.com>
2018-11-27 23:18:31 -0800

    Added Hand Class

--- Blackjack/BlackJack.cs
+++ Blackjack/BlackJack.cs
@@ -10,5 +10,9 @@ namespace Blackjack
     {
         // TODO: Add methods and member variables needed to organize the card game aspect of the program
         // TODO: Integrate Hand, Deck, and AI classes into this class 
+
+
+
+        public void dealHand() { }
     }
 }
--- Blackjack/Hand.cs
+++ Blackjack/Hand.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace Blackjack
 {
-    class Hand
+    /// <summary>
+    /// This class will hold cards as they are dealt out in sequence from the Deck.
+    /// 
+    /// </summary>
+    class Hand : Deck
     {
+        private int handValue { get; set; }
+
+
+        /// <summary>
+        /// This is a hellper method which will recur everytime a card is dealt to check for a 
+        /// busted state, then it will trigger an event to signal this event
+        /// </summary>
+        private void checkBusted()
+        {
+            if (handValue > 21) { }
+            //TODO: trigger event on GamePage to signify a busted hand
+        }
     }
 }
