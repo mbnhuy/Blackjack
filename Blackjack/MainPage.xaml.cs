230cd34f339d8f208041e56600d2e73812d5b12b
bveltre <baveltre@gmail.com>
2018-11-27 22:29:54 -0800

    Added Carlo's Main Page

--- Blackjack/MainPage.xaml.cs
+++ Blackjack/MainPage.xaml.cs
@@ -12,7 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
-
+using MUXC = Microsoft.UI.Xaml.Controls;
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
 namespace Blackjack
@@ -28,26 +28,65 @@ namespace Blackjack
         }
 
         /// <summary>
-        /// This routinue will be triggered when the user presses the Start button from the main menu.
-        /// It will change the screen from the main menu to the main game screen where the user can begin playing 
-        /// the game. Need to look up the code which will allow switching between views in c#.
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void StartGame(object sender, RoutedEventArgs e)
-        {
-            // TODO: Write the method which will change to the GamePage
-            this.Frame.Navigate(typeof(GamePage));
-        }
-
-        /// <summary>
         /// This method will exit the game when the user clicks it from the main menu.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        private void QuitGame(object sender, RoutedEventArgs e)
+        private void Quit_Game(object sender, RoutedEventArgs e)
         {
-            // TODO: Write the method to exit the program
+            Application.Current.Exit();
+        }
+
+        /// <summary>
+        /// Pressing settings button will bring up the options menu.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            Settings.IsPaneOpen = !Settings.IsPaneOpen;
+        }
+
+        /// <summary>
+        /// Toggles option one depending on which radio button is pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Toggle_Option_1(object sender, RoutedEventArgs e)
+        {
+            // TODO: FIGURE OUT OPTION 1 AND CHANGE NAME IN XAML
+        }
+
+        // TODO: KEEP OPTIONS CHECKED BETWEEN PAGE CHANGES.
+
+        /// <summary>
+        /// Toggles option two depending on which radio button is pressed.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Toggle_Option_2(object sender, RoutedEventArgs e)
+        {
+            // TODO: FIGURE OUT OPTION 2 AND CHANGE NAME IN XAML
+        }
+
+        /// <summary>
+        /// TODO: Set to start mode 1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Start_Mode1(object sender, RoutedEventArgs e)
+        {
+            Frame.Navigate(typeof(GamePage));
+        }
+
+        private void Start_Mode2(object sender, RoutedEventArgs e)
+        {
+            // TODO: MODE 2
+        }
+
+        private void Close_Flyout(object sender, RoutedEventArgs e)
+        {
+            QuitFlyout.Hide();
         }
     }
 }
