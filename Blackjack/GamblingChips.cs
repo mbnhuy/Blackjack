589f7a79c63953112eca57b02e5643ce208e9f48
Huy Tran <hgtran@sdsu.edu>
2018-11-28 14:05:32 -0800

    Added GamblingChips Class for our Gambling Tokens

---
+++ Blackjack/GamblingChips.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blackjack
+{
+	class GamblingChips
+	{
+		private string[] GambleTokens;
+		private const int OneDollar Token = 1;
+
+	public GamblingChips()
+		{
+			GambleTokens = TokensForGamble;
+		}
+	}
+}
