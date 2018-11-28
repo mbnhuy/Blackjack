355040df7665c5c51de1447cae88294c9e743341
bveltre <baveltre@gmail.com>
2018-11-27 22:15:32 -0800

    Added new empty class files

--- Blackjack/BlackJack.cs
+++ Blackjack/BlackJack.cs
@@ -9,5 +9,6 @@ namespace Blackjack
     class BlackJack
     {
         // TODO: Add methods and member variables needed to organize the card game aspect of the program
+        // TODO: Integrate Hand, Deck, and AI classes into this class 
     }
 }
